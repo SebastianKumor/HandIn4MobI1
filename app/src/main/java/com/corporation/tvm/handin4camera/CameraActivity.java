e29a08165310f86e9c179ba86a8ee826913f044f
SebastianKumor <basty191120@gmail.com>
2015-10-04 13:50:26 +0200

    camera wroking

--- app/src/main/java/com/corporation/tvm/handin4camera/CameraActivity.java
+++ app/src/main/java/com/corporation/tvm/handin4camera/CameraActivity.java
@@ -16,16 +16,16 @@ import android.widget.FrameLayout;
 import java.util.List;
 
 
-public class CameraActivity extends AppCompatActivity  {
+public class CameraActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
     private static final String TAG = "CameraInfoTag";
-
 
 
     private Camera mCamera;
     private SurfaceView surfview;
     private SurfaceHolder surfaceHolder;
     private Button picButton;
+
 
     List<Camera.Size> mCameraSizes;
     Camera.Size optimalSize;
@@ -40,46 +40,156 @@ public class CameraActivity extends AppCompatActivity  {
 
 
 
-        FrameLayout frame =(FrameLayout)findViewById(R.id.camera_activity_frame);
-                frame.addView(new CustomSurfaceView(this));
-        // Create an instance of Camera
+        mCamera =getCameraInstance();
+        surfview = (SurfaceView)findViewById(R.id.surf_view);
+        surfaceHolder = surfview.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 
-     //   mCamera =getCameraInstance();
-//        surfview = (SurfaceView)findViewById(R.id.surf_view);
-//        surfaceHolder = surfview.getHolder();
-//        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        setCameraDisplayOrientation(CameraActivity.this, findFrontFacingCameraID(), mCamera);
+        picButton = (Button) findViewById(R.id.camera_button);
 
+        // surfaceHolder.addCallback(CameraActivity.this);
 
-        picButton=(Button)findViewById(R.id.camera_button);
-
-       // surfaceHolder.addCallback(CameraActivity.this);
-
-       // mCameraSizes=mCamera.getParameters().getSupportedPreviewSizes();
     }
 
 
 
 
 
-    public static Camera getCameraInstance(){
+
+
+    // getting optimal size for camera
+
+    public void refreshCamera() {
+
+        if (surfaceHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+
+            mCamera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
+        }
+
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+//        Camera.Parameters parameters = mCamera.getParameters();
+//        parameters.setPreviewSize(optimalSize.width, optimalSize.height);
+//        mCamera.setParameters(parameters);
+//        mCamera.startPreview();
+        // start preview with new settings
+
+        try {
+            mCamera.setPreviewDisplay(surfaceHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+
+        }
+    }
+
+    private int findFrontFacingCameraID() {
+        int cameraId = -1;
+        // Search for the front facing camera
+        int numberOfCameras = Camera.getNumberOfCameras();
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                Log.d(TAG, "Camera found");
+                cameraId = i;
+                break;
+            }
+        }
+        return cameraId;
+    }
+
+    public static void setCameraDisplayOrientation(CameraActivity activity,
+                                                   int cameraId, Camera camera) {
+        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
+        android.hardware.Camera.getCameraInfo(cameraId, info);
+        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
+        int degrees = 0;
+        switch (rotation) {
+            case Surface.ROTATION_0:
+                degrees = 0;
+                break;
+            case Surface.ROTATION_90:
+                degrees = 90;
+                break;
+            case Surface.ROTATION_180:
+                degrees = 180;
+                break;
+            case Surface.ROTATION_270:
+                degrees = 270;
+                break;
+        }
+
+        int result;
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            result = (info.orientation + degrees) % 360;
+            result = (360 - result) % 360;  // compensate the mirror
+        } else {  // back-facing
+            result = (info.orientation - degrees + 360) % 360;
+        }
+
+        camera.setDisplayOrientation(result);
+    }
+
+
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+        try {
+            // open the camera
+            mCamera = Camera.open();
+        } catch (RuntimeException e) {
+
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+
+        try {
+            // The Surface has been created, now tell the camera where to draw
+            // the preview.
+            mCamera.setPreviewDisplay(this.surfaceHolder);
+            mCamera.startPreview();
+        } catch (Exception e) {
+            // check for exceptions
+            System.err.println(e);
+            return;
+        }
+    }
+
+    public static Camera getCameraInstance() {
         Camera c = null;
         try {
             c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             // Camera is not available (in use or does not exist)
         }
         return c; // returns null if camera is unavailable
     }
 
-    public static CameraActivity getActivity(){
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
 
+        mCamera.stopPreview();
+        mCamera.release();
 
-        return getActivity();
+        mCamera = null;
     }
 
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        refreshCamera();
+    }
 
-    // getting optimal size for camera
 
 
 
--- app/src/main/java/com/corporation/tvm/handin4camera/CustomSurfaceView.java
+++ app/src/main/java/com/corporation/tvm/handin4camera/CustomSurfaceView.java
@@ -15,7 +15,7 @@ import java.util.List;
 /**
  * Created by sebastiankumor on 03/10/15.
  */
-public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+public class CustomSurfaceView extends SurfaceView  {
 
     private static final String TAG = "CameraTag";
 
@@ -29,7 +29,7 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
         super(context);
 
 
-        mCamera=getCameraInstance();
+//        mCamera=getCameraInstance();
 
 
         mCameraSizes=mCamera.getParameters().getSupportedPreviewSizes();
@@ -37,7 +37,7 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
         // Install a SurfaceHolder.Callback so we get notified when the
         // underlying surface is created and destroyed.
         holder = getHolder();
-        holder.addCallback(this);
+      //  holder.addCallback(this);
         // deprecated setting, but required on Android versions prior to 3.0
         holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
     }
@@ -51,178 +51,11 @@ public class CustomSurfaceView extends SurfaceView implements SurfaceHolder.Call
         super(context, attrs, defStyle);
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        final int widthSpecMode = View.MeasureSpec.getMode(widthMeasureSpec);
-        final int heightSpecMode = View.MeasureSpec.getMode(heightMeasureSpec);
-
-        if (mCameraSizes != null) {
-            optimalSize = getOptimalPreviewSize(mCameraSizes, widthSpecMode, heightSpecMode);
-        }
-
-        float ratio;
-        if(optimalSize.height >= optimalSize.width)
-            ratio = (float) optimalSize.height / (float) optimalSize.width;
-        else
-            ratio = (float) optimalSize.width / (float) optimalSize.height;
-
-        // One of these methods should be used, second method squishes preview slightly
-        setMeasuredDimension(widthSpecMode, (int) (widthSpecMode * ratio));
-//        setMeasuredDimension((int) (width * ratio), height);
-    }
-
-    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
-        final double ASPECT_TOLERANCE = 0.1;
-        double targetRatio = (double) h / w;
-
-        if (sizes == null)
-            return null;
-
-        Camera.Size optimalSize = null;
-        double minDiff = Double.MAX_VALUE;
-
-        int targetHeight = h;
-
-        for (Camera.Size size : sizes) {
-            double ratio = (double) size.height / size.width;
-            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE)
-                continue;
-
-            if (Math.abs(size.height - targetHeight) < minDiff) {
-                optimalSize = size;
-                minDiff = Math.abs(size.height - targetHeight);
-            }
-        }
-
-        if (optimalSize == null) {
-            minDiff = Double.MAX_VALUE;
-            for (Camera.Size size : sizes) {
-                if (Math.abs(size.height - targetHeight) < minDiff) {
-                    optimalSize = size;
-                    minDiff = Math.abs(size.height - targetHeight);
-                }
-            }
-        }
-
-        return optimalSize;
-    }
-
-    public void refreshCamera() {
-
-        if (holder.getSurface() == null) {
-            // preview surface does not exist
-            return;
-        }
-
-        // stop preview before making changes
-        try {
-
-            mCamera.stopPreview();
-        } catch (Exception e) {
-            // ignore: tried to stop a non-existent preview
-        }
-
-        setCameraDisplayOrientation(CameraActivity.getActivity(), findFrontFacingCameraID(), mCamera);
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-
-        Camera.Parameters parameters = mCamera.getParameters();
-        parameters.setPreviewSize(optimalSize.width, optimalSize.height);
-        mCamera.setParameters(parameters);
-        mCamera.startPreview();
-        // start preview with new settings
-
-        try {
-            mCamera.setPreviewDisplay(holder);
-            mCamera.startPreview();
-        } catch (Exception e) {
-
-        }
-    }
-
-    private int findFrontFacingCameraID() {
-        int cameraId = -1;
-        // Search for the front facing camera
-        int numberOfCameras = Camera.getNumberOfCameras();
-        for (int i = 0; i < numberOfCameras; i++) {
-            Camera.CameraInfo info = new Camera.CameraInfo();
-            Camera.getCameraInfo(i, info);
-            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-                Log.d(TAG, "Camera found");
-                cameraId = i;
-                break;
-            }
-        }
-        return cameraId;
-    }
-
-    public static void setCameraDisplayOrientation(CameraActivity activity,
-                                                   int cameraId, android.hardware.Camera camera) {
-        android.hardware.Camera.CameraInfo info = new android.hardware.Camera.CameraInfo();
-        android.hardware.Camera.getCameraInfo(cameraId, info);
-        int rotation = activity.getWindowManager().getDefaultDisplay().getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0: degrees = 0; break;
-            case Surface.ROTATION_90: degrees = 90; break;
-            case Surface.ROTATION_180: degrees = 180; break;
-            case Surface.ROTATION_270: degrees = 270; break;
-        }
-
-        int result;
-        if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-            result = (info.orientation + degrees) % 360;
-            result = (360 - result) % 360;  // compensate the mirror
-        } else {  // back-facing
-            result = (info.orientation - degrees + 360) % 360;
-        }
-
-        camera.setDisplayOrientation(result);
-    }
 
 
-    public void surfaceCreated(SurfaceHolder holder) {
-        // The Surface has been created, now tell the camera where to draw the preview.
-        try {
-            // open the camera
-            mCamera = Camera.open();
-        } catch (RuntimeException e) {
 
-            // check for exceptions
-            System.err.println(e);
-            return;
-        }
 
-        try {
-            // The Surface has been created, now tell the camera where to draw
-            // the preview.
-            mCamera.setPreviewDisplay(this.holder);
-            mCamera.startPreview();
-        } catch (Exception e) {
-            // check for exceptions
-            System.err.println(e);
-            return;
-        }
-    }
 
-    public static Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        // empty. Take care of releasing the Camera preview in your activity.
-    }
-
-    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        refreshCamera();
-    }
 
 
 }
