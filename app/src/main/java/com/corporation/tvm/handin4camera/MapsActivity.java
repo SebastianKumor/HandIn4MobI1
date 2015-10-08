e6f8f709940a665d3a9e0651fad5c055b1901432
Leon243 <roman243@gmail.com>
2015-10-08 19:41:24 +0200

    GPS fix

--- app/src/main/java/com/corporation/tvm/handin4camera/MapsActivity.java
+++ app/src/main/java/com/corporation/tvm/handin4camera/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.corporation.tvm.handin4camera;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
 
 import com.google.android.gms.location.LocationListener;
@@ -34,7 +37,16 @@ public class MapsActivity extends android.support.v4.app.FragmentActivity implem
         LocationManager locationManager;
         String context = Context.LOCATION_SERVICE;
         locationManager = (LocationManager)getSystemService(context);
-
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            Toast.makeText(this, "GPS is Enabled in your devide", Toast.LENGTH_SHORT).show();
+        }else{
+            buildAlertMessageNoGps();
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            Toast.makeText(this, "Network is Enabled in your devide", Toast.LENGTH_SHORT).show();
+        }else{
+            buildAlertMessageNoGps();
+        }
         String provider = LocationManager.GPS_PROVIDER;
         try{
             loc = locationManager.getLastKnownLocation(provider);
@@ -49,45 +61,63 @@ public class MapsActivity extends android.support.v4.app.FragmentActivity implem
         String context = Context.LOCATION_SERVICE;
 
         if (loc != null) {
+            if ( (!locman.isProviderEnabled( LocationManager.GPS_PROVIDER )) || (!locman.isProviderEnabled( LocationManager.NETWORK_PROVIDER )) ) {
+                buildAlertMessageNoGps();
+            }
             double lat = loc.getLatitude();
             double lot = loc.getLongitude();
             String text = lat + "," + lot;
             String[] separated = text.split(",");
             latitude = Double.parseDouble(separated[0]);
             longitude = Double.parseDouble(separated[1]);
-            custom = new LatLng(latitude,longitude);
-            map.addMarker(new MarkerOptions().position(custom).title("Marker in Custom"));
-            map.moveCamera(CameraUpdateFactory.newLatLng(custom));
-        }
-        if (loc == null)
-        {
-            try{
-                locman = (LocationManager)getSystemService(context);
-            }catch (Exception e) {
-                e.printStackTrace();
+            if (latitude == 0 && longitude == 0)
+            {
+                custom = new LatLng(0,0);
+                map.addMarker(new MarkerOptions().position(custom).title("Marker in Default"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(custom));
             }
-            if(locman != null){
-                try{
-                    locman.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,0,0, (android.location.LocationListener)this);
-                }
-                catch (SecurityException se){
-                    se.printStackTrace();
-                }
-                try{
-                    loc = locman.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                }
-                catch (SecurityException se){
-                    se.printStackTrace();
-                }
-                double lat = loc.getLatitude();
-                double lot = loc.getLongitude();
-                String text = lat + "," + lot;
-                String[] separated = text.split(",");
-                latitude = Double.parseDouble(separated[0]);
-                longitude = Double.parseDouble(separated[1]);
-                custom = new LatLng(latitude,longitude);
+            else {
+                custom = new LatLng(latitude, longitude);
                 map.addMarker(new MarkerOptions().position(custom).title("Marker in Custom"));
                 map.moveCamera(CameraUpdateFactory.newLatLng(custom));
+            }
+        }
+        if (loc == null) {
+            try {
+                locman = (LocationManager) getSystemService(context);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if (!locman.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                buildAlertMessageNoGps();
+            } else {
+                if (locman != null) {
+                    try {
+                        locman.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (android.location.LocationListener) this);
+                    } catch (SecurityException se) {
+                        se.printStackTrace();
+                    }
+                    try {
+                        loc = locman.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    } catch (SecurityException se) {
+                        se.printStackTrace();
+                    }
+                    double lat = loc.getLatitude();
+                    double lot = loc.getLongitude();
+                    String text = lat + "," + lot;
+                    String[] separated = text.split(",");
+                    latitude = Double.parseDouble(separated[0]);
+                    longitude = Double.parseDouble(separated[1]);
+                    if (latitude == 0 && longitude == 0) {
+                        custom = new LatLng(0, 0);
+                        map.addMarker(new MarkerOptions().position(custom).title("Marker in Default"));
+                        map.moveCamera(CameraUpdateFactory.newLatLng(custom));
+                    } else {
+                        custom = new LatLng(latitude, longitude);
+                        map.addMarker(new MarkerOptions().position(custom).title("Marker in Custom"));
+                        map.moveCamera(CameraUpdateFactory.newLatLng(custom));
+                    }
+                }
             }
         }
     }
@@ -110,5 +140,23 @@ public class MapsActivity extends android.support.v4.app.FragmentActivity implem
         Toast.makeText(getApplicationContext(), "Gps Enabled",
                 Toast.LENGTH_SHORT).show();
     }
-
+    private void buildAlertMessageNoGps() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Your GPS seems to be disabled, do you want to enable it?")
+                .setCancelable(false)
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(@SuppressWarnings("unused") final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+                    }
+                })
+                .setNegativeButton("No/Already Activated", new DialogInterface.OnClickListener() {
+                    public void onClick(final DialogInterface dialog, @SuppressWarnings("unused") final int id) {
+                        dialog.cancel();
+                        finish();
+                        startActivity( new Intent(MapsActivity.this,MapsActivity.class));
+                    }
+                });
+        final AlertDialog alert = builder.create();
+        alert.show();
+    }
 }
